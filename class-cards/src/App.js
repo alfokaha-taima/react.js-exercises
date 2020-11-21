62da445e1b7a85bb809a0afd8cd390bee48120aa
unknown <taima.fokaha@gmail.com>
2020-11-21 23:20:27 +0200

    React-Cards

---
+++ class-cards/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Card from './components/Card'
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className="header">Class A</h1>
+      <Card />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ class-cards/src/components/Card.js
@@ -0,0 +1,233 @@
+import React,{useState} from 'react'
+import './Card.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FaFacebook, FaLinkedinIn} from 'react-icons/fa';
+import { FaGithub } from 'react-icons/fa';
+import { FaLinkedin } from 'react-icons/fa';
+
+
+
+function Card(){
+
+  
+
+    return(
+        <div className="flex">
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars3.githubusercontent.com/u/71769659?s=460&u=f79c2bdf655b094836b60d58a15f4a478631e827&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+    <h3>Taima Al-fokaha</h3>
+    <h4>Electronics and communication Engineering</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a href="https://web.facebook.com/taima.alfokaha"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/alfokaha-taima"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/taima%E2%80%99-al-fokaha%E2%80%99/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+    
+    
+   
+            </div>
+        </div>
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars3.githubusercontent.com/u/60710271?s=400&u=4beabb3ede0403226fde0a59fbc995c15fbaf6c3&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+    <h3>Dana Toughoj</h3>
+    <h4>Managment Information System</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a className="icons" href="https://web.facebook.com/toughoj"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/danatoughoj"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/dana-toughoj/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+    
+    
+            </div>
+        </div>
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars2.githubusercontent.com/u/71584331?s=400&u=59e7fe47593366524b5851eeb4a6e4cd4fba1799&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+    <h3>Jenan Musallam</h3>
+    <h4>Managment Information System</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a className="icons" href="https://web.facebook.com/jenan.musallam"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/Jenanmusallam"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/jenan-musallam/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+   
+            </div>
+        </div>
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars2.githubusercontent.com/u/71583926?s=460&u=290e3c7e943e0eea053b4bb1c89607207d67163a&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+    <h3>Angham M.Bani-Younis</h3>
+    <h4>Software Engineering</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a className="icons" href="https://github.com/Angham-Baniyounes"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/Angham-Baniyounes"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/angham-baniyounes/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+    
+            </div>
+        </div>
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars2.githubusercontent.com/u/71770879?s=460&u=2b363f174c196ad0a14ecb5a3a3dbe37550d952b&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+            <h3>Maysam Turk</h3>
+    <h4>Human Nutration & Dietetics</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a className="icons" href="https://web.facebook.com/maysam.turk"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/MaysamTurk"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/maysamturk/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+    
+            </div>
+        </div>
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars3.githubusercontent.com/u/71584804?s=400&u=3b941316bf72ff47d8fef9577006e2b54ba5462e&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+    <h3>Razan Zuaiter</h3>
+    <h4>Communication Engineering</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a className="icons" href="https://web.facebook.com/Razan.zaiter.13"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/Razan-Zuaiter"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/razan-zuaiter/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+    
+            </div>
+        </div>
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars3.githubusercontent.com/u/71769638?s=460&u=1a470e089de00fb48d18cefefbc1776e0c808b78&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+    <h3>Lubna almaaweed</h3>
+    <h4>Financial and Banking information System</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a className="icons" href="https://web.facebook.com/lubna.amjad.5621"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/lubna-almaaweed"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/lubna-almaaweed/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+    
+            </div>
+        </div>
+        <div className="Card">
+            <div className="upper-container">
+                <div className="image-container">
+                    <img src="https://avatars2.githubusercontent.com/u/71769566?s=400&u=c066c010c2e4eb481f2d0ef22fd736ca9150ed34&v=4" alt="" height="100px" width="100px"/>
+
+                </div>
+            </div>
+            <div className="lower-container">
+    <h3>Hala A.Al-Hyasat</h3>
+    <h4>communication Engineering</h4>
+    <p>web and mobile developer at Orange Coding Academy</p>
+    <div className="icons">
+        <a className="icons" href="https://web.facebook.com/HalaA.AlHyasat"> 
+        <FaFacebook  className="icons"> </FaFacebook> 
+        </a>
+        <a className="icons" href="https://github.com/HalaHyasat"> 
+        <FaGithub  className="icons"> </FaGithub> 
+        </a><a className="icons" href="https://www.linkedin.com/in/hala-alhyasat/"> 
+        <FaLinkedin  className="icons"> </FaLinkedin> 
+        </a>
+    
+    
+    </div>
+    
+            </div>
+        </div>
+        </div>
+    )
+}
+
+
+  
+export default Card;
